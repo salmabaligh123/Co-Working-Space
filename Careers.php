0e4a9a2b5c94f14f4ebfe9b13754a07a7facb211
MartinaRaafat77 <119627856+MartinaRaafat77@users.noreply.github.com>
2022-12-20 15:07:29 +0200

    Add files via upload

---
+++ Careers.php
@@ -0,0 +1,131 @@
+ <html>
+<head>
+    <link rel="stylesheet" href="Careers.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <?php include "header.php" ?>
+</head>
+<html lang="zxx" class="theme-light">
+<body>
+        <div class="page-banner-area">
+            <div class="container">
+                <div class="page-banner-content">
+                    <h2>OPEN POSITIONS</h2>
+                    <h3>Join Our Team</h3>
+                    <p style=" color:white;font-family: Arial, Helvetica, sans-serif;text-align: center;"> We’re proud to have a resilient team who’s behind the success that we’ve achieved throughout the past years.
+                        We’re now looking for passionate individuals to join our fast-growing team and become part of our success story.</p>
+                    
+                    <ul class="pages-list" style="text-align: center;">
+                        <li style="color: white;font-family: Arial, Helvetica, sans-serif;text-align: center;"><a href="Home.php">Home</a>
+                         <<  Workspaces
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="coworking-space-area pt-100 pb-70">
+            <div class="container">
+                <div class="section-title">
+                    <h2>BRANCH LEAD</h2>
+                    <br>
+                    <br>
+                    <h4 style="margin-left: -1000px;font-weight:bold;">Role Summary:</h4>
+                    <br>
+                    <p>You will be part of the Operations team, who works together to improve the working environment and business processes of Meccano. The main aim is developing operational capacities, strengthening customer’s relationships and ensuring consistent growth in revenue.</p>
+                    <br>
+                    <br>
+                    <h4 style="margin-left: -940px;font-weight:bold;">What you will be doing:</h4>
+                    <p style=" text-decoration: underline;">Operations Management</p>
+                    <ul>
+                    <li>Responsible for MQR venue - (assets, maintenance, logistics, canteen suppliers, inventory. infrastructure, etc.) In addition to building and maintaining the relationship with the branch main stakeholders (neighbors, security and landlord, etc.) to secure smooth continued daily operations.</li>
+                    <li>Accurate recording – (In charge of revising the documented daily bookings, shifts payments ‘revenues & costs’).</li>
+                    <li>Supervise the space assets quality, cleanness and report any issue to be worked out immediately.</li>
+                    </ul>
+                    <p style=" text-decoration: underline;">Sales Management</p>
+                    <ul>
+                        <li>Work to achieve branch financial targets and plan Sales strategies ahead.</li>
+                        <li>Study and analyze branch Sales performance and apply improvements if needed.</li>
+                        <li>Work alongside Sales Lead to provide support in generating leads.</li>
+                        </ul>
+                    <p style=" text-decoration: underline;">Customers Satisfaction</p>
+                    <ul>
+                        <li>Handle customer’s needs, requests & complains to maintain the highest satisfaction rates.</li>
+                        <li>Build and maintain good relations with work stakeholders.</li>
+                        <li>Provide and secure smooth running of the supporting functions services based on the customer’s needs.</li>
+                     </ul>
+                     <p style=" text-decoration: underline;">Resources Management</p>
+                    <ul>
+                        <li>Monitor and evaluate the Operations team to provide prompt guidance and supervision in order to help achieving branch’s goals, as well as, securing team member’s continuous development.</li>
+                        <li>Work alongside the supporting functions "Sales, Accounting, Marketing, and People" to fulfill Operation needs.</li>
+                     </ul>
+                     <br>
+                     <br>
+                     <h4 style="margin-left: -900px;font-weight:bold;">What we're looking for:</h4>
+                     <ul>
+                        <li>Not less than 2 years of experience in Operations and Sales.</li>
+                        <li>Strong in communication and able to handle different mindsets.</li>
+                        <li>Factors to succeed in Operations; you need to be detail oriented, and hold knowledge/practice on Crisis Management.</li>
+                        <li>Being people oriented will help you win team & customers.</li>
+                        <li>Background on Finance to help you in reading and analyzing branch financial performance.</li>
+                        <li>Microsoft office/Google Knowledge.</li>
+                        <li>Extras - Familiar with Startups environment and its requirements.</li>
+                     </ul>
+                     <br>
+                     <br>
+                     <br>
+                     <h2>Community Officer</h2>
+                     <br>
+                     <br>
+                     <h4 style="margin-left: -940px;font-weight:bold;">What you will be doing:</h4>
+                     <p style=" text-decoration: underline;">Operations Management</p>
+                     <ul>
+                     <li>Ensure excellent customer arrival experience and warm welcoming.</li>
+                     <li>Accurate recording of Meccano Members check-in/out on application.</li>
+                     <li>Accurate recording of daily revenues & costs on system.</li>
+                     <li>Accurate shift closure, & cash handling according to the aligned Accounting process.</li>
+                     <li>Manage daily reservations via phone or offline, with accurate recording of all reservation inquires.</li>
+                     <li>Enforce a level of uncompromising cleanliness, including managing the service resource to execute daily cleaning at the highest standard.</li>
+                     <li>Conduct quality control walkthroughs, to address immediate issues or potential future issues.</li>
+                     <li>Answer the “walk-ins” customers and assist them in what they need.</li>
+                     </ul>
+                     <p style=" text-decoration: underline;">Members Management</p>
+                     <ul>
+                         <li>Develop relationships with Members and proactively gather information on their needs to identify type of service MQR could offer to help them achieve their goals.</li>
+                         <li>Identify and execute opportunities to connect members with each other.</li>
+                         <li>Meet with Members to resolve issues and manage their expectations.</li>
+                         </ul>
+                     <p style=" text-decoration: underline;">Sales Management</p>
+                     <ul>
+                         <li>Achieve the assigned Sales target.</li>
+                         <li>Contact Leads on become a member sheet.</li>
+                         <li>Tour and sell space to new members by pitching MQR service offerings.</li>
+                         <li>Encourage current members to renew or upgrade their memberships.</li>
+                         <li>Based on your day to day experience with customers, gather insights & feedback on what could be done for better sales, and report to Branch Lead.</li>
+                     </ul>
+                      <br>
+                      <h4 style="margin-left: -900px;font-weight:bold;">What we're looking for:</h4>
+                      <ul>
+                         <li>Understanding of Operations/Customer Management. </li>
+                         <li>Understanding of Sales Management. </li>
+                         <li>Strong organization/communication skills.</li>
+                         <li>Microsoft Office knowledge.</li>
+                         <li>Good English</li>
+                      </ul>
+                      <br>
+                      <h4 style="margin-left: -950px;font-weight:bold;">Work Conditions</h4>
+                      <ul>
+                         <li>7 hrs. Per day (Night/Morning) shift. </li>
+                         <li>2 days’ vacation (Rotational).</li>
+                      </ul>
+                      <button type="submit" class="but">Apply Here</button>
+                    </div>
+            </div>
+        </div>
+
+</body>
+<footer>
+        <?php include "footer.php" ?>
+</footer>
+</html>
--- footer.php
+++ footer.php
@@ -1,97 +1,115 @@
 <html>
-<!-- Start Footer Area -->
-<footer class="footer-area pt-100">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-3 col-sm-6">
-                <div class="single-footer-widget">
-                    <div class="widget-logo">
-                        <a href="HomePage.php">
-                            <img src="images/logo-2.png" alt="image">
-                        </a>
+    <head>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+    </head>
+    <body>
+
+    <footer class="footer">
+        <div class="box-container">
+            <div class="box">
+                <div class="row">
+                    <div class="col-sm-4">
+                        <div class="footer-column footer-about">
+                        <h3 class="footer-title"> About </h3>
+                        <p class="footer-about-text">Welcome to the best Co-working space in New York where you'll feel yourself HOME. </p>
                     </div>
-                    <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin lorem quis bibendum auctor nisi elit consequat ipsum thnec sagittis sem nibh id elit.</p>
-
-                    <ul class="widget-info">
-                        <li>
-                            <i class='bx bxs-phone'></i>
-                            <a href="tel:0023567890">+00 234 567 890</a>
-                        </li>
-
-                        <li>
-                            <i class='bx bx-envelope-open'></i>
-                            <a href="/cdn-cgi/l/email-protection#6f1c1f1a1d092f08020e0603410c0002"><span class="__cf_email__" data-cfemail="addeddd8dfcbedcac0ccc4c183cec2c0">[email&#160;protected]</span></a>
-                        </li>
-                    </ul>
                 </div>
-            </div>
+                    <div class="col-sm-4">
+                        <div class="footer-column footer-explore">
+                            <h3 class="footer-title"> Explore </h3>
+                            <ul class="footer-explore-list list-unstyled">
+                                <li><a href=".html">Home</a></li>
+                                <li><a href=".html">Rooms</a></li>
+                                <li><a href=".html">Courses</a></li>
+                                <li><a href=".html">Careers</a></li>
+                                <li><a href="findUs.html">Find us</a></li>
+                                <li><a href="contact.html">Contact us</a></li>
+                            </ul>
+                        </div>
+                    </div>
 
-            <div class="col-lg-3 col-sm-6">
-                <div class="single-footer-widget">
-                    <h3>Our Company</h3>
-
-                    <ul class="footer-links-list">
-                        <li><a href="contact.php">Our Location</a></li>
-                        <li><a href="about.php">About Our Mission</a></li>
-                        <li><a href="about.php">Careers</a></li>
-                        <li><a href="events-details.php">Event Venue</a></li>
-                        <li><a href="blog.php">Our News</a></li>
-                    </ul>
-                </div>
-            </div>
-
-            <div class="col-lg-3 col-sm-6">
-                <div class="single-footer-widget">
-                    <h3>Our Services</h3>
-
-                    <ul class="footer-links-list">
-                        <li><a href="services-details.php">Coworking</a></li>
-                        <li><a href="services-details.php">Dedicated Office</a></li>
-                        <li><a href="services-details.php">Hot Desking</a></li>
-                        <li><a href="services-details.php">Virtual Office</a></li>
-                        <li><a href="workspaces.php">Meeting Rooms</a></li>
-                    </ul>
-                </div>
-            </div>
-
-            <div class="col-lg-3 col-sm-6">
-                <div class="single-footer-widget">
-                    <h3>All Support</h3>
-
-                    <ul class="footer-links-list">
-                        <li><a href="services-details.php">Help Desk</a></li>
-                        <li><a href="events-booking.php">Book A Tour</a></li>
-                        <li><a href="contact.php">Know Us</a></li>
-                        <li><a href="services-details.php">Virtual Office</a></li>
-                    </ul>
-
-                    <ul class="widget-social">
-                        <li>
-                            <a href="https://twitter.com/?lang=en" target="_blank">
-                                <i class='bx bxl-twitter'></i>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://www.facebook.com/" target="_blank">
-                                <i class='bx bxl-facebook'></i>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://www.instagram.com/" target="_blank">
-                                <i class='bx bxl-instagram-alt'></i>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://www.linkedin.com/" target="_blank">
-                                <i class='bx bxl-linkedin'></i>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
+                    <div class="col-sm-4">
+                        <div class="footer-column footer-contact">
+                            <h3 class="footer-title">Contact</h3>
+                            <p class="footer-contact-text">1616 Egypt, Cairo 10001<br>Egypt</p>
+                            <div class="footer-contact-info">
+                                <p class="footer-contact-phone"><span class="flaticon-call"></span> 855 100 4444</p>
+                                <p class="footer-contact-mail">info@mecano.com</p>
+                            </div>
+                        </div>
             </div>
         </div>
-    </div>
+            </div>
+        
+    </footer>
+</body>
 
-</footer>
-<!-- End Footer Area -->
 </html>
+
+<style>
+    .footer {
+        margin-top: 0%;
+        align-items: center;
+        margin-left: 5%;
+    }
+    .footer .box-container{
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(25rem, 1fr));
+        gap:1.5rem;
+        
+    }
+    
+    .footer .box-container .box h3{
+        padding:.5rem 0;
+        font-size: 2.5rem;
+        color:var(--black);
+    }
+    li
+    {
+        color:black;
+    }
+    .footer .box-container .box a{
+        display: block;
+        padding:.5rem 0;
+        font-size: 1.5rem;
+        color:var(--light-color);
+    }
+    
+    .footer .box-container .box a:hover{
+        color:var(--green);
+        text-decoration: underline;
+    }
+    
+    .footer .credit{
+        text-align: center;
+        border-top: .1rem solid rgba(0,0,0,.1);
+        font-size: 2rem;
+        color:var(--black);
+        padding:.5rem;
+        padding-top: 1.5rem;
+        margin-top: 1.5rem;
+    }
+    
+    .footer .credit span{
+        color:var(--green);
+    }
+    
+    .loader-container{
+        position: fixed;
+        top:0; left:0;
+        height:100%; 
+        width:100%;
+        z-index: 10000;
+        background:#fff;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        overflow: hidden;
+    }
+    </style>
+
+
+
