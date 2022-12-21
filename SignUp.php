c761da3089d3e9fd4ffa84325f10fd86be178594
Salma Baligh <97367130+salmabaligh123@users.noreply.github.com>
2022-12-21 23:13:46 +0200

    Update and rename SignUp.html to SignUp.php

---
+++ SignUp.php
@@ -0,0 +1,47 @@
+<html>
+<head>
+    <?php include "header.php" ?>
+</head>
+<body>
+    <!-- Start SignUp Area -->
+    <div class="login-area ptb-100">
+        <div class="container">
+            <div class="login-form">
+                <h2>Sign Up</h2>
+
+                <form>
+                    <div class="form-group">
+                        <label>First Name</label>
+                        <input type="text" class="form-control" placeholder="FirstName">
+                    </div>
+
+                    <div class="form-group">
+                        <label>Last Name</label>
+                        <input type="text" class="form-control" placeholder="LastName">
+                    </div>
+
+                    <div class="form-group">
+                        <label>Email</label>
+                        <input type="email" class="form-control" placeholder="Email">
+                    </div>
+
+                    <div class="form-group">
+                        <label>Mobile Number</label>
+                        <input type="email" class="form-control" placeholder="Number">
+                    </div>
+
+                    <div class="form-group">
+                        <label>Password</label>
+                        <input type="password" class="form-control" placeholder="Password">
+                    </div>
+
+                    <button type="submit" class="default-btn">SignUp <span></span></button>
+                </form>
+            </div>
+        </div>
+    </div>
+    <!-- End SignUp Area -->
+    <?php include "footer.php" ?>
+</body>
+
+</html>
