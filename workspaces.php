b26558625613eb26a83e90190e00a35fbd29df06
Salma Baligh <97367130+salmabaligh123@users.noreply.github.com>
2022-12-21 21:30:48 +0200

    Update and rename Reservation.php to workspaces.php

---
+++ workspaces.php
@@ -0,0 +1,302 @@
+<html>
+<head>
+  <?php include "header.php" ?>
+</head>
+<body>
+<!-- Start Coworking Space Area -->
+ <div class="coworking-space-area pt-100 pb-70">
+            <div class="container">
+                <div class="section-title">
+                    <span>Coworking Space</span>
+                    <h2>Our Modern Office Spaces Are Simply Stunning & Comfortable</h2>
+                </div>
+
+                <div class="row justify-content-center">
+                    <div class="col-lg-4 col-md-6">
+                        <div class="single-coworking-space">
+                            <div class="image">
+                                <img src="coworking-space/coworking-1.jpg" alt="image">
+
+                                <div class="number">01</div>
+                                <div class="hover-number">01</div>
+                            </div>
+
+                            <div class="content">
+                                <h3>
+                                    Small Team
+                                </h3>
+                                <span>Single Desk & Hot Desk</span>
+                            </div>
+
+                            <div class="hover-content">
+                                <h3>
+                                    Small Team
+                                </h3>
+                                <span>Single Desk & Hot Desk</span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6">
+                        <div class="single-coworking-space">
+                            <div class="image">
+                                <img src="coworking-space/coworking-2.jpg" alt="image">
+
+                                <div class="number">02</div>
+                                <div class="hover-number">02</div>
+                            </div>
+
+                            <div class="content">
+                                <h3>
+                                    Office Space
+                                </h3>
+                                <span>Single Desk & Hot Desk</span>
+                            </div>
+
+                            <div class="hover-content">
+                                <h3>
+                                    Office Space
+                                </h3>
+                                <span>Single Desk & Hot Desk</span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6">
+                        <div class="single-coworking-space">
+                            <div class="image">
+                                <img src="coworking-space/coworking-3.jpg" alt="image">
+
+                                <div class="number">03</div>
+                                <div class="hover-number">03</div>
+                            </div>
+
+                            <div class="content">
+                                <h3>
+                                    Conference Room
+                                </h3>
+                                <span>Single Desk & Hot Desk</span>
+                            </div>
+
+                            <div class="hover-content">
+                                <h3>
+                                    Conference Room
+                                </h3>
+                                <span>Single Desk & Hot Desk</span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6">
+                        <div class="single-coworking-space">
+                            <div class="image">
+                                <img src="coworking-space/coworking-4.jpg" alt="image">
+
+                                <div class="number">04</div>
+                                <div class="hover-number">04</div>
+                            </div>
+
+                            <div class="content">
+                                <h3>
+                                    Private Office
+                                </h3>
+                                <span>Single Desk & Hot Desk</span>
+                            </div>
+
+                            <div class="hover-content">
+                                <h3>
+                                    Private Office
+                                </h3>
+                                <span>Single Desk & Hot Desk</span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6">
+                        <div class="single-coworking-space">
+                            <div class="image">
+                                <img src="coworking-space/coworking-5.jpg" alt="image">
+
+                                <div class="number">05</div>
+                                <div class="hover-number">05</div>
+                            </div>
+
+                            <div class="content">
+                                <h3>
+                                    Custom Space
+                                </h3>
+                                <span>Single Desk & Hot Desk</span>
+                            </div>
+
+                            <div class="hover-content">
+                                <h3>
+                                    Custom Space
+                                </h3>
+                                <span>Single Desk & Hot Desk</span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6">
+                        <div class="single-coworking-space">
+                            <div class="image">
+                                <img src="coworking-space/coworking-6.jpg" alt="image">
+
+                                <div class="number">06</div>
+                                <div class="hover-number">06</div>
+                            </div>
+
+                            <div class="content">
+                                <h3>
+                                    Meeting Space
+                                </h3>
+                                <span>Single Desk & Hot Desk</span>
+                            </div>
+
+                            <div class="hover-content">
+                                <h3>
+                                    Meeting Space
+                                </h3>
+                                <span>Single Desk & Hot Desk</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Coworking Space Area -->
+           
+        <!-- Start Main Banner Area -->
+        <div class="main-banner-area">
+            <div class="main-banner-item-box">
+                <div class="container">
+                    <div class="main-banner-content">
+                        <h1>Make Your Reservation</h1>
+
+                        <div class="banner-search-form">
+                            <div class="checkbox-list">
+                                <div class="form-check">
+                                    <input type="checkbox" class="form-check-input" id="private-offices">
+                                    <label class="form-check-label" for="private-offices">Private Offices</label>
+                                </div>
+
+                                <div class="form-check">
+                                    <input type="checkbox" class="form-check-input" id="desks">
+                                    <label class="form-check-label" for="desks">Desks</label>
+                                </div>
+
+                                <div class="form-check">
+                                    <input type="checkbox" class="form-check-input" id="meetings">
+                                    <label class="form-check-label" for="meetings">Meetings & Conference Rooms</label>
+                                </div>
+
+                                <div class="form-check">
+                                    <input type="checkbox" class="form-check-input" id="virtual-offices">
+                                    <label class="form-check-label" for="virtual-offices">Virtual Offices</label>
+                                </div>
+                            </div>
+
+                            <form>
+                                <div class="row align-items-center">
+                                    <div class="col-lg-4 col-md-6">
+                                        <div class="form-group">
+                                            <label><i class='bx bx-search'></i></label>
+                                            <input type="text" class="form-control" placeholder="Search Space...">
+                                        </div>
+                                    </div>
+
+                                    <div class="col-lg-3 col-md-6">
+                                        <div class="form-group">
+                                            <label><i class='bx bx-calendar'></i></label>
+                                            <input type="text" class="form-control" placeholder="Start Date">
+                                        </div>
+                                    </div>
+    
+                                    <div class="col-lg-3 col-md-6">
+                                        <div class="form-group">
+                                            <div class="select-box">
+                                                <select>
+                                                    <option>No. of Seat</option>
+                                                    <option>100</option>
+                                                    <option>500</option>
+                                                    <option>600</option>
+                                                    <option>1000</option>
+                                                    <option>200</option>
+                                                    <option>780</option>
+                                                    <option>850</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </div>
+    
+                                    <div class="col-lg-2 col-md-6">
+                                        <div class="submit-btn">
+                                            <button type="submit">Search</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+
+                            <ul class="banner-list">
+                                <li>
+                                    <i class='bx bx-world'></i> 56 Countries
+                                </li>
+                                <li>
+                                    <i class='bx bxs-network-chart'></i> 5600+ Coworking Spaces
+                                </li>
+                                <li>
+                                    <i class='bx bxs-group'></i> 4.5 Million Users
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="features-area-three">
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-3 col-sm-6">
+                            <div class="single-box">
+                                <img src="features/features-1.png" alt="image">
+                                <h3>
+                                    Event Space
+                                </h3>
+                            </div>
+                        </div>
+    
+                        <div class="col-lg-3 col-sm-6">
+                            <div class="single-box">
+                                <img src="features/features-2.png" alt="image">
+                                <h3>
+                                    High Speed Wifi
+                                </h3>
+                            </div>
+                        </div>
+    
+                        <div class="col-lg-3 col-sm-6">
+                            <div class="single-box">
+                                <img src="features/features-3.png" alt="image">
+                                <h3>
+                                    Customize Space
+                                </h3>
+                            </div>
+                        </div>
+    
+                        <div class="col-lg-3 col-sm-6">
+                            <div class="single-box">
+                                <img src="features/features-4.png" alt="image">
+                                <h3>
+                                   Snacks & Coffee
+                                </h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Main Banner Area -->
+  <?php include "footer.php" ?>
+</body>
+
+</html>
